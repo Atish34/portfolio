8e106dbac0740086fc6dd1a76d6a1b4bd8ac3a4e
Atish34 <atishudawant07@gmail.com>
2025-01-06 11:05:09 +0530

    deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(express.static("dist"))
 
-app.use(cors({origin:'http://localhost:5173' ,credentials:true}))
+app.use(cors({origin:'https://portfolio-ehrk.onrender.com' ,credentials:true}))
 
 app.use("/api",require("./routes/My.routes"))
 
